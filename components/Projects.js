0e56e867bd828d37b1cfb4b476412760f063549a
Yannick Roy <ymyannick@gmail.com>
2022-09-30 09:10:47 +0800

    Deployment doesn't like the HTML like path... noew trying 'importing' the resources for Next Image

--- components/Projects.js
+++ components/Projects.js
@@ -2,6 +2,17 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 import projectImgPlaceHolder from "../public/projects/project_placeholder_code.png";
 
+import projectImgNeurofuntrade from ("/../public/projects/neurofuntrade.png");
+import projectImgCrytpoUI from ("/../public/projects/cryptoui.png");
+import projectImgNeuroTechX from ("/../public/projects/neurotechx.png");
+import projectImgDLEEGReview from ("/../public/projects/dleegreview.png");
+import projectImgNeureka from ("/../public/projects/neureka.png");
+import projectImgNeuraldrift from ("/../public/projects/neuraldrift.png");
+import projectImgDeepfakes from "/projects/deeplearning/deepfakes";
+import projectImgStar from "/../public/projects/star.png";
+import projectImgSynapets from "/../public/projects/synapsets.png";
+import projectImgStudentClubs from "/../public/projects/studentclubs.gif";
+
 const Projects = () => {
   return (
     <div id="projects" className="w-full">
@@ -13,28 +24,28 @@ const Projects = () => {
         <div className="grid gap-8 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
           <ProjectItem
             title="NeuroFunTrade"
-            backgroundImg="/../public/projects/neurofuntrade.png"
+            backgroundImg={projectImgNeurofuntrade}
             projectUrl="/projects/blockchain/neurofuntrade"
             tech="Blockchain, NTF, Web3"
             year="2022"
           />
           <ProjectItem
             title="Crypto UI"
-            backgroundImg="/../public/projects/cryptoui.png"
+            backgroundImg={projectImgCrytpoUI}
             projectUrl="/projects/blockchain/crypto-ui"
             tech="Blockchain, Python, PyQt5"
             year="2022"
           />
           <ProjectItem
             title="NeuroTechX"
-            backgroundImg="/../public/projects/neurotechx.png"
+            backgroundImg={projectImgNeuroTechX}
             projectUrl="/projects/neurotech/neurotechx"
             tech="NeuroTech, Community"
             year="2015 - Present"
           />
           <ProjectItem
             title="DL-EEG Review"
-            backgroundImg="/../public/projects/dleegreview.png"
+            backgroundImg={projectImgDLEEGReview}
             projectUrl="/projects/neurotech/dl-eeg-review"
             tech="Deep Learning, EEG, Python"
             year="2019"
@@ -48,42 +59,42 @@ const Projects = () => {
           />
           <ProjectItem
             title="Neureka Challenge"
-            backgroundImg="/../public/projects/neureka.png"
+            backgroundImg={projectImgNeureka}
             projectUrl="/projects/neurotech/neureka-challenge"
             tech="Deep Learning, EEG"
             year="2020"
           />
           <ProjectItem
             title="NeuralDrift"
-            backgroundImg="/../public/projects/neuraldrift.png"
+            backgroundImg={projectImgNeuraldrift}
             projectUrl="/projects/neurotech/neuraldrift"
             tech="EEG, Machine Learning, Python"
             year="2015"
           />
           <ProjectItem
             title="Deepfakes"
-            backgroundImg="/../public/projects/deepfakes.gif"
+            backgroundImg={projectImgDeepfakes}
             projectUrl="/projects/deeplearning/deepfakes"
             tech="Deep Learning"
             year="2019"
           />
           <ProjectItem
             title="StaR"
-            backgroundImg="/../public/projects/star.png"
+            backgroundImg={projectImgStar}
             projectUrl="/projects/neurotech/star-eeglab"
             tech="EEG, Matlab"
             year="2015"
           />
           <ProjectItem
             title="NeuroTech StudentClubs"
-            backgroundImg="/../public/projects/studentclubs.gif"
+            backgroundImg={projectImgStudentClubs}
             projectUrl="/projects/neurotech/student-clubs"
             tech="NeuroTech, Community"
             year="2015"
           />
           <ProjectItem
             title="SynapsETS"
-            backgroundImg="/../public/projects/synapsets.png"
+            backgroundImg={projectImgSynapets}
             projectUrl="/projects/neurotech/synapsets"
             tech="NeuroTech, Community"
             year="2015"
