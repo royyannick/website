5a01c4d271b0137104f6c6483a78a72d9e85c27e
Yannick Roy <ymyannick@gmail.com>
2023-09-17 00:13:00 +0800

    Updated URL for NeuroFunTrade

--- pages/projects/blockchain/neurofuntrade.js
+++ pages/projects/blockchain/neurofuntrade.js
@@ -31,7 +31,7 @@ const NeuroFunTrade = () => {
           <p>NeuroFunTrade</p>
           <h2>Overview</h2>
           <p className="py-4">Description Coming Soon...</p>
-          <Link href="https://neuronft.netlify.app/sell-nft">
+          <Link href="https://neurofun-nft-marketplace.vercel.app/">
             <a target="_blank" rel="noopener noreferrer">
               <button className="px-8 py-2 mt-4 mr-8">Website</button>
             </a>
